31f55b889d5fca74def157ed34a3948dc7509da6
sa21b030 <sa21b030@technikum-wien.at>
2023-10-08 22:13:48 +0200

    MVC implementiert

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
+var notes = [];
+
 function createNote() {
-    if (document.getElementById("title").value!="" || document.getElementById("text").value!="") {
-        let article=createArticle();
+    let title = document.getElementById("title");
+    let text = document.getElementById("text");
+    let color = document.getElementById("color");
+    if (title.value != "" || text.value != "") {
+        let article = createArticle(generateId(title.value, text.value), title.value, text.value, color.value);
         document.getElementById("notes").appendChild(article);
-        localStorage.setItem("notes", document.getElementById("notes").innerHTML);
+        notes.push({id: article.id, title: title.value, text: text.value, color: color.value});
+        localStorage.setItem("notes", JSON.stringify(notes));
+        title.value = "";
+        text.value = "";
+        title.focus();
     } else {
         alert("Bitte zumindest Text oder Titel angeben");
     }
 }
 
-function createArticle() {
-    let title=document.createElement("h1");
-    title.innerHTML=document.getElementById("title").value;
-    let text=document.createElement("p");
-    text.innerHTML=document.getElementById("text").value;
-    let article=document.createElement("article");
-    article.setAttribute("style", "background-color:"+document.getElementById("color").value+";");
+function createArticle(idValue, titleValue, textValue, colorValue) {
+    let title = document.createElement("h1");
+    title.textContent = titleValue;
+    let text = document.createElement("p");
+    text.textContent = textValue;
+    let article = document.createElement("article");
+    article.setAttribute("id", idValue);
+    article.setAttribute("style", "background-color:"+colorValue+";");
+    article.setAttribute("data-color", colorValue);
     article.appendChild(title);
     article.appendChild(text);
     article.appendChild(createDeleteButton());
@@ -24,17 +35,42 @@ function createArticle() {
 function createDeleteButton() {
     let btn=document.createElement("button");
     btn.setAttribute("onclick", "deleteArticle(this)");
-    btn.innerHTML="Delete";
+    btn.textContent = "Delete";
     return btn;   
 }
 
 function deleteArticle(button) {
-    let thisArticle=button.parentNode;
+    let thisArticle = button.parentNode;
     thisArticle.parentNode.removeChild(thisArticle);
-    localStorage.setItem("notes", document.getElementById("notes").innerHTML);  
+    let index = notes.findIndex((element) => element.id === thisArticle.getAttribute("id"));
+    if (index !== -1) {
+       notes = notes.slice(0, index).concat(notes.slice(index+1));
+    }
+    localStorage.setItem("notes", JSON.stringify(notes));  
 }
 
-window.onload=init;
 function init() {
-    document.getElementById("notes").innerHTML=localStorage.getItem("notes");
+    notes = JSON.parse(localStorage.getItem("notes"));
+    const insert = document.getElementById("notes");
+    notes.forEach(element => {
+       insert.appendChild(createArticle(element.id, element.title, element.text, element.color));
+    });
+    const title = document.getElementById("title");
+    title.addEventListener("keydown", handleKeyDown);
+    const text = document.getElementById("text");
+    text.addEventListener("keydown", handleKeyDown);
+}
+
+function handleKeyDown(event) {
+    if (event.key === "Enter" && (event.target.id === "title" || event.ctrlKey) ) {
+        createNote();
+    }    
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    init();
+});
+
+function generateId(title, text, length = 10) {
+     return CryptoJS.SHA256(title + text + new Date()).toString().substring(0, length);
 }
